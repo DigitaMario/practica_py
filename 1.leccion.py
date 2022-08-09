2898c3bc55b8ac2e6f019c375b94b24ad8c2a67e
mario camayo <mariocamayo17@gmail.com>
2022-08-08 22:39:19 -0500

    primer practica con python

---
+++ 1.leccion.py
@@ -0,0 +1,12 @@
+# print('hola mario')
+# print('hola mario')
+numero_entero =250
+# print(numero_entero)
+numero_decimal = 4.5
+# print(numero_decimal)
+numero_entero +=100%6
+# print(numero_entero)
+numero_entero =250
+numero_entero =250
+num = numero_decimal + numero_entero
+print(num)
